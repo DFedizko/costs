5f0b8d64cd2f912c391cdd2cba68a7276a60ec08
Pedro Fedizko <fedizkopedro@gmail.com>
2025-04-30 18:36:09 -0300

    Adicionando validação no Form de adicionar projeto

--- src/components/pages/NewProject/index.jsx
+++ src/components/pages/NewProject/index.jsx
@@ -1,12 +1,23 @@
 import styles from "./NewProject.module.css";
-import ProjectForm from "../../project/ProjectForm";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import ProjectForm from "../../project/ProjectForm";
+import Message from "../../layout/Message";
 
 const NewProject = () => {
-
+    const [message, setMessage] = useState();
     const navigate = useNavigate();
 
     const createPost = (project) => {
+
+        if (!project.name.trim() || !project.budget || project.budget <= 0 || !project.category) {
+            setTimeout(() => {
+                setMessage("Por favor preencha todos os campos com valores válidos!");
+            }, 0);
+            setMessage("");
+            return;
+        }
 
         // initialize cost and services
         project.cost = 0;
@@ -32,6 +43,7 @@ const NewProject = () => {
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar seus serviços</p>
             <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+            {message && <Message type="error" msg={message} />}
         </div>
     );
 }
